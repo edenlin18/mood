bd8216ef44d7fe5a175f0f386183c3abeda606d2
Ruochen Yang <elmerdanny.yang@gmail.com>
2016-02-04 07:04:29 +0000

    added feed template and feed page

--- routes/feedPage.js
+++ routes/feedPage.js
@@ -5,6 +5,47 @@
 exports.view = function(req, res) {
 	var mood = req.query.mood;
 	console.log(mood);
-	res.render('feedPage',{"imgName":"/img/"+mood+"background.jpg"});
+	res.render('feedPage',{"imgName":"/img/"+mood+"background.jpg",
+		'feeds' :[{
+			'title':'test',
+			'content' : 'blahlajkfldjklfjskljdksljkflsfds',
+			'id':mood
+		},
+		{
+			'title':'test',
+			'content' : 'blahlajkfldjklfjskljdksljkflsfds',
+			'id':mood
+		},
+		{
+			'title':'test',
+			'content' : 'blahlajkfldjklfjskljdksljkflsfds',
+			'id':mood
+		},
+		{
+			'title':'test',
+			'content' : 'blahlajkfldjklfjskljdksljkflsfds',
+			'id':mood
+		},
+		{
+			'title':'test',
+			'content' : 'blahlajkfldjklfjskljdksljkflsfds',
+			'id':mood
+		},
+		{
+			'title':'test',
+			'content' : 'blahlajkfldjklfjskljdksljkflsfds',
+			'id':mood
+		},
+		{
+			'title':'test',
+			'content' : 'blahlajkfldjklfjskljdksljkflsfds',
+			'id':mood
+		},
+		{
+			'title':'test',
+			'content' : 'blahlajkfldjklfjskljdksljkflsfds',
+			'id':mood
+		},]
+	});
 	
 };
